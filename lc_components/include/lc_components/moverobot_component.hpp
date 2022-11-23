bd06950ee60ddbbf32d61e3a0c8c3562e1ad7f3a
kimsooyoung <tge1375@naver.com>
2022-11-23 20:59:37 +0900

    File formatted

--- lc_components/include/lc_components/moverobot_component.hpp
+++ lc_components/include/lc_components/moverobot_component.hpp
@@ -4,18 +4,16 @@
 #ifndef COMPOSITION__MOVEROBOT_COMPONENT_HPP_
 #define COMPOSITION__MOVEROBOT_COMPONENT_HPP_
 
+#include "geometry_msgs/msg/twist.hpp"
 #include "lc_components/visibility_control.h"
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/twist.hpp"
 
-namespace lc_components
-{
+namespace lc_components {
 
-class MoveRobot : public rclcpp::Node
-{
+class MoveRobot : public rclcpp::Node {
 public:
   COMPOSITION_PUBLIC
-  explicit MoveRobot(const rclcpp::NodeOptions & options);
+  explicit MoveRobot(const rclcpp::NodeOptions &options);
 
 protected:
   void on_timer();
@@ -26,6 +24,6 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
-}  // namespace composition
+} // namespace lc_components
 
-#endif  // COMPOSITION__MOVEROBOT_COMPONENT_HPP_
+#endif // COMPOSITION__MOVEROBOT_COMPONENT_HPP_
